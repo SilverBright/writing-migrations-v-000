dff317856e5741463e791d1d343afbec83346517
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-30 21:42:14 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,8 +2,7 @@ class CreateStudents < ActiveRecord::Migration[5.1]
 
   def change
     create_table :students do |t|
-      t.string :grade
-      t.string :birthdate
+      t.string :name
     end
   end
 
