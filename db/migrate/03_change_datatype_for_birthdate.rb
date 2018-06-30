8fe67ed7beb965b9e32767f35143348a94389e64
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-30 21:53:34 +0000

    automatically backed up by learn

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,10 @@
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+
+  def change
+    change_column(students, birthdate, string) :students do |t|
+      t.integer :grade
+      t.string :birthdate
+    end
+  end
+
+end
